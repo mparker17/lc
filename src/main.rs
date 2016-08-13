3005b77a799afb3b78d2c4fa9035addf08a6ee05
M Parker <mparker17@536298.no-reply.drupal.org>
2016-08-13 17:56:16 -0400

    Accept a file to read from.

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,35 @@ extern crate clap;
 
 use std::io;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 use std::collections::HashMap;
 use clap::App;
+use clap::Arg;
 
 mod lc;
 
 fn main() {
-    App::new("lc - letter count")
+    let matches = App::new("lc - letter count")
         .version(crate_version!())
         .author(crate_authors!())
-        .about("The lc utility displays the number of instances of each letter contained in standard input to the standard output. Newline characters are not counted.")
-        .usage("lc < $file_to_parse")
+        .about("The lc utility displays the number of instances of each letter contained in each input, or standard input (if no file is specified) to the standard output. Newline characters are not counted.")
+        .arg(Arg::with_name("file")
+           .help("Sets the input file to use"))
         .get_matches();
 
-    let stdin = io::stdin();
     let mut counter : HashMap<String, u64> = HashMap::new();
-    let lines = stdin.lock().lines();
+
+    // If a file to read from was passed in, use it. Otherwise, use standard
+    // input.
+    let file : Box<io::Read> = match matches.value_of("file") {
+        Some(filename) => Box::new(File::open(filename).unwrap()),
+        None => Box::new(io::stdin()),
+    };
+    let reader = BufReader::new(file);
 
     // Loop through all the lines, and count the graphemes in each one.
-    for line in lines {
+    for line in reader.lines() {
         lc::count_graphemes_in_string(line.unwrap(), &mut counter);
     }
 

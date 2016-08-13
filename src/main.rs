392e87bf8e9cd03441005198afc435f1773cc2be
M Parker <mparker17@536298.no-reply.drupal.org>
2016-08-13 16:12:40 -0400

    Split code to count graphemes in strings into own function.

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,28 @@ fn print_summary(counter: HashMap<String, u64>) {
 }
 
 /// Counts graphemes in a set of lines.
-fn count_graphemes_in_lines(lines: Lines<std::io::StdinLock>, counter: &mut HashMap<String, u64>) {
+fn count_graphemes_in_lines(lines: Lines<std::io::StdinLock>, mut counter: &mut HashMap<String, u64>) {
     for line in lines {
         let line_string = line.unwrap();
 
-        // Loop through each character in the current line...
-        for grapheme in UnicodeSegmentation::graphemes(line_string.as_str(), true) {
-            // If the character we are looking at already exists in the counter
-            // hash, get its value. Otherwise, start a new counter at zero.
-            let count = counter.entry(grapheme.to_string()).or_insert(0);
+        count_graphemes_in_string(line_string, &mut counter);
+    }
+}
 
-            // In either case, increment the counter.
-            *count += 1;
-        }
+/// Counts the number of graphemes in a string.
+fn count_graphemes_in_string(to_parse: String, mut counter: &mut HashMap<String, u64>) {
+    // The UnicodeSegmentation library can only handle string slices. Convert
+    // the input string to that before parsing.
+    let to_parse_str = to_parse.as_str();
+
+    // Loop through each character in the current string...
+    for grapheme in UnicodeSegmentation::graphemes(to_parse_str, true) {
+        // If the character we are looking at already exists in the counter
+        // hash, get its value. Otherwise, start a new counter at zero.
+        let count = counter.entry(grapheme.to_string()).or_insert(0);
+
+        // In either case, increment the counter.
+        *count += 1;
     }
 }
 

d3f0b5f1492f5c75d97fa1735d050fe64d822d15
M Parker <mparker17@536298.no-reply.drupal.org>
2016-08-13 17:21:08 -0400

    Use clap library to improve CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+#[macro_use]
+extern crate clap;
+
 use std::io;
 use std::io::prelude::*;
 use std::collections::HashMap;
+use clap::App;
 
 mod lc;
 
 fn main() {
+    App::new("lc - letter count")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("The lc utility displays the number of instances of each letter contained in standard input to the standard output. Newline characters are not counted.")
+        .usage("lc < $file_to_parse")
+        .get_matches();
+
     let stdin = io::stdin();
     let mut counter : HashMap<String, u64> = HashMap::new();
     let lines = stdin.lock().lines();

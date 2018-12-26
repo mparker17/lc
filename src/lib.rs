7281941fc4df4dfcdc7adc1815a6f096201fd177
M Parker <mparker17@users.noreply.github.com>
2018-12-26 14:52:47 -0500

    Cargo clippy suggested making hashmap algorithm generic.

--- src/lib.rs
+++ src/lib.rs
@@ -21,14 +21,17 @@ use self::unicode_segmentation::UnicodeSegmentation;
 use std::collections::HashMap;
 
 /// Prints a summary of the contents of a grapheme counter.
-pub fn print_summary(counter: &HashMap<String, u64>) {
+pub fn print_summary<S: ::std::hash::BuildHasher>(counter: &HashMap<String, u64, S>) {
     for (key, val) in counter.iter() {
         println!("{}: {}", key, val);
     }
 }
 
 /// Counts all the graphemes in a string.
-pub fn count_graphemes_in_string(to_parse: &str, counter: &mut HashMap<String, u64>) {
+pub fn count_graphemes_in_string<S: ::std::hash::BuildHasher>(
+    to_parse: &str,
+    counter: &mut HashMap<String, u64, S>,
+) {
     // Loop through each character in the current string...
     for grapheme in UnicodeSegmentation::graphemes(to_parse, true) {
         // If the character we are looking at already exists in the counter
